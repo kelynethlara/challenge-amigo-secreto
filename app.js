5515059d7e293fe7ad2ad78f536a47cd9bb7c928
KelynethLara <kelyneth1331@gmail.com>
2025-03-17 22:58:39 -0500

    No permitir nombres en blanco y alerta

--- app.js
+++ app.js
@@ -5,3 +5,9 @@
  // Función para agregar nombres al array y mostrarlos
  function agregarAmigo() {
      let input = document.getElementById("amigo");
+     let nombre = input.value.trim(); // Eliminar nombres en blanco
+ 
+     if (nombre === "") {
+         alert("No puedes agregar un nombre vacío. Porfavor escribe algo.");
+         return;
+     }
