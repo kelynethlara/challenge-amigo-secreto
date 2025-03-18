054dc153239c9eaa1c524f9d844e1f0e5fba7fd7
KelynethLara <kelyneth1331@gmail.com>
2025-03-17 22:57:54 -0500

    Xrear lista y capturar nombres

---
+++ app.js
@@ -0,0 +1,7 @@
+@@ -0,0 +1,43 @@
+ // Lista para almacenar los nombres
+ let nombres = [];
+ 
+ // Función para agregar nombres al array y mostrarlos
+ function agregarAmigo() {
+     let input = document.getElementById("amigo");
